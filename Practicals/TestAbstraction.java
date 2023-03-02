6b6216f81a59a22c1e582a46e277ce86afd12a33
Nikita Mali <126570356+nikitasmali@users.noreply.github.com>
2023-03-02 17:31:28 +0530

    Add files via upload

---
+++ Practicals/TestAbstraction.java
@@ -0,0 +1,14 @@
+package corejava;
+
+public class TestAbstraction {
+
+	public static void main(String[] args)
+	{
+		//creating object of class AbstractionExample
+		TestAbstraction1 a=new TestAbstraction1(1);
+		
+		
+
+	}
+
+}
---
+++ Practicals/TestAbstraction1.java
@@ -0,0 +1,13 @@
+package corejava;
+
+public class TestAbstraction1 extends AbstractionExample
+{
+	private double salary;
+
+	public TestAbstraction1(double salary) 
+	{
+		super();
+		this.salary = salary;
+	}
+
+}
