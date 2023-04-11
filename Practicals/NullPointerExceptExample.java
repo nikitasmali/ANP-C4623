d031e1c0660647fb1ca8ec5b1ccef13336c91d06
Nikita Mali <126570356+nikitasmali@users.noreply.github.com>
2023-04-11 17:43:18 +0530

    Add files via upload

---
+++ Practicals/NullPointerExceptExample.java
@@ -0,0 +1,19 @@
+package collections;
+
+public class NullPointerExceptExample {
+	  public static void main(String[] args)
+	    {
+	       String str = null;
+	    	
+	        try 
+	        {
+	            // Attempt to access a method on a null object
+	        	int l = str.length(); // This will throw a NullPointerException
+	        
+	        } 
+		        catch (NullPointerException e)
+		        {
+		            System.out.println("Caught NullPointerException: " + e.getMessage());
+		        }
+	    }
+}
